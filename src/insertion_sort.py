4433edb3e97f8bc83f7f771bbfc1269ec592c9be
Farshad Nassiri <farshadnassiri@gmail.com>
2025-10-18 19:41:45 +0330

    completing insertion and selection functions

---
+++ src/insertion_sort.py
@@ -0,0 +1,14 @@
+
+def insertion_sort(list: List[int]) -> List[int]:
+    
+    for i in range(1, len(list)):
+        key = list[i]
+        j = i - 1
+        while j >= 0 and key < list[j]:
+            list[j + 1] = list[j]
+            j -= 1
+        list[j + 1] = key
+    return list
+
+if __name__ == "__main__":
+    assert insertion_sort([3, 1, 2]) == [1, 2, 3]
