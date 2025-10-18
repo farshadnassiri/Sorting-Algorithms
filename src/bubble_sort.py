7a792a04db8f7e1a9c16f5df2040c14771c86588
Farshad Nassiri <farshadnassiri@gmail.com>
2025-10-18 17:47:27 +0330

    first commit

---
+++ src/bubble_sort.py
@@ -0,0 +1,8 @@
+def bubble_sort(list):
+    n=len(list)
+    for i in range(n):
+        for j in range(0,n-i-1):
+            if list[j] > list[j+1]:
+                list[j], list[j+1] = list[j+1], list[j]
+    return list                    
+                
